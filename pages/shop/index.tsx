878c4a9ae255fee2c95941d23e5fe24560675357
ZPerekoty <zakharperekoty@gmail.com>
2022-09-24 23:57:34 +0300

    Ver 1.2.2

---
+++ pages/shop/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+import { ShopPage } from "../../container";
+
+const Shop: NextPage = (): JSX.Element => {
+    return (
+        <>
+            <ShopPage />
+        </>
+    );
+};
+
+export default Shop;
